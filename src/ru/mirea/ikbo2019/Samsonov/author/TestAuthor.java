835e44a63d640bc92ff570386df9396103807d80
Vladimir <volodya_samsonov@inbox.ru>
2020-09-17 22:45:08 +0300

    Initial commit

---
+++ src/ru/mirea/ikbo2019/Samsonov/author/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.ikbo2019.Samsonov.author;
+public class TestAuthor {
+
+    public static void main(String[] args) {
+        Author author = new Author('M', "Tolstoy", "tolsoy@war.piece");
+        System.out.println(author);
+    }
+}
